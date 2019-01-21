a7d3bef4849434b912d2c4d336d4c3b6e133e7f4
jasenfinch <jsf9@aber.ac.uk>
2019-01-21 20:33:38 +0000

    added isAlive() and processes() functions

---
+++ R/isAlive.R
@@ -0,0 +1,23 @@
+#' alive
+#' @description Check if a host is active.
+#' @param host host address
+#' @param port port to check
+#' @importFrom pingr ping ping_port
+#' @examples isAlive('www.google.com')
+#' @export
+
+isAlive <- function(host,port = NULL){
+  if (is.null(port)) {
+    response <- ping(host,count = 1)
+  } else {
+    response <- ping_port(host,port = port)
+  }
+  
+  if (is.na(response)) {
+    alive <- FALSE
+  } else {
+    alive <- TRUE
+  }
+  
+  return(alive)
+}
---
+++ R/processes.R
@@ -0,0 +1,29 @@
+#' processes
+#' @description Return system processes
+#' @param root should root processes be returned
+#' @examples 
+#' processes()
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_split_fixed
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr bind_rows filter
+#' @importFrom purrr map
+#' @export
+
+processes <- function(root = TRUE){
+  a <- system('ps -aux',intern = T) %>%
+    map(~{
+      str_split_fixed(.,pattern = '\\s+',n = 11) %>%
+        as_tibble()
+    }) %>%
+    bind_rows()
+  
+  colnames(a) <- a[1,]
+  a <- a[-1,]
+  
+  if (isFALSE(root)) {
+    a %>%
+      filter(USER != 'root')
+  }
+}
+
