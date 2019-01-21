69bc00b990530c0cce35df58f320c47a84680609
jasenfinch <jsf9@aber.ac.uk>
2019-01-21 21:39:46 +0000

    added memory functions

---
+++ R/memory.R
@@ -0,0 +1,61 @@
+#' @importFrom magrittr set_colnames
+#' @export
+memoryInfo <- function(units = 'KB'){
+  mem <- 'cat /proc/meminfo' %>%
+    system(intern = TRUE) %>%
+    map(~{
+      str_split_fixed(.,':',2) %>%
+        set_colnames(c('Type','Size')) %>%
+        as_tibble() 
+      }) %>%
+    bind_rows() %>%
+    mutate(Size = str_remove_all(Size,'\\s'),
+           Size = str_remove_all(Size,'[:alpha:]') %>% 
+             as.numeric() %>%
+             {. * 1024}
+             )
+  if (units != 'B') {
+    mem <- mem %>%
+      mutate(Size = convertUnits(Size,'B',units))
+  }
+  return(mem)
+}
+
+#' @export
+
+convertUnits <- function(value,from,to,factor = 1024){
+  convert <- tibble(Unit = c('B','KB','MB','GB'),Value = c(0:3)) %>%
+    filter(Unit == from | Unit == to)
+  
+  from <- convert$Value[convert$Unit == from]
+  to <- convert$Value[convert$Unit == to]
+  
+  if (from < to) {
+    value / factor ^ (to - from)
+  } else {
+    value * factor ^ (from - to)
+  }
+}
+
+#' @export
+
+availableMemory <- function(units = 'KB'){
+ memoryInfo(units = units) %>%
+    filter(Type == 'MemAvailable') %>%
+    .$Size
+}
+
+#' @importFrom stringr str_remove_all
+#' @export
+
+totalMemory <- function(units = 'KB'){
+  memoryInfo(units = units) %>%
+    filter(Type == 'MemTotal') %>%
+    .$Size
+}
+
+#' @export
+
+usedMemory <- function(units = 'KB'){
+  totalMemory(units = units) - availableMemory(units = units)
+}
